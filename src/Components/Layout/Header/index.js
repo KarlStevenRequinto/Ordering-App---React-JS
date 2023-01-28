243e24accd04a47443d54ef0017bc91e6622abab
KarlStevenRequinto <lerdrobott@gmail.com>
2023-01-29 01:21:55 +0800

    added cart btn,cart icon svg

--- src/Components/Layout/Header/index.js
+++ src/Components/Layout/Header/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import mealsImage from "../../../Assets/meals.jpg";
+import HeaderCartButton from "../HeaderCartButton";
 import  "./styles.css"
 function Header() {
   return (
     <>
       <header className="header">
         <h1>Ordering App</h1>
-        <button>Cart</button>
+        <HeaderCartButton/>
       </header>
       <div className="main-image">
         <img src={mealsImage} alt="Meals Background Here!"/>

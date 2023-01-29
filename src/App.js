db98c45dcccc33e42b92553bd35b726cfbc9933c
KarlStevenRequinto <lerdrobott@gmail.com>
2023-01-29 21:58:36 +0800

    added Meals and Card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./Components/Layout/Header";
+import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
-    <div>
-      <Header></Header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Meals/>
+      </main>
+    </>
   );
 }
 
